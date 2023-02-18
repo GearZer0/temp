a6a47ac8b9f9eaebefca5f56c5a0f6148f238af3
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-02-18 17:22:10 +0800

    Update messagetrace3.ps1

--- messagetrace3.ps1
+++ messagetrace3.ps1
@@ -19,53 +19,23 @@ param([string]$RecipientAddress,$Subject)
     $i=1
     $z=10
    Write-Host "MESSAGE TRACE CALL FOR USER $($RecipientAddress)"
+   Write-host "The Subject is $($Subject)"
     while($z -gt 0)
            {
               $date=(get-date).AddDays(-$z)
               $enddate= get-date
-              Write-host "Scanning Message Trace for Recipeint $($RecipientAddress)  for the $($subject) $z  days back"
+              #Write-host "Scanning Message Trace for Recipeint $($RecipientAddress)  for the $($subject) $z  days back"
               while($i  -lt $pages)
                   {     
                      $mail+=Get-MessageTrace -Page $i -PageSize 5000 -SenderAddress $RecipientAddress -startdate $date -enddate $enddate
                       $i++
-                    Write-host "Message Trace going for user $($RecipientAddress) "
-                    
-                    }
-             $z=$z-1
-           }
-           $mail |export-csv $Resultfile -Append
-           $mails=$mail |?{$_.subject -match $Subject}
-           
-           
-           
-          
-$value=$null           
-$value=@()
-           if($mails.count -eq 0)
-           {
-           $value=$null
-           }
+                      Write-host "Message Trace going for user $($RecipientAddress) for page # $i "
+                   }
+            $z=$z-1	
 
-           if($mails.count -eq 1)
-           {
-           $obj=new-object psobject
-           $obj |add-member -NotePropertyname SenderAddress -NotePropertyValue $mails.SenderAddress
-           $obj |add-member -NotePropertyname RecipientAddress -NotePropertyValue $mails.RecipientAddress 
-           $value+=$obj
-           }
-         if($mails.count -gt 1)
-           { 
-            foreach($ml in $mails)
-            {
-             $obj=new-object psobject
-             $obj |add-member -NotePropertyname SenderAddress -NotePropertyValue $ml.SenderAddress
-             $obj |add-member -NotePropertyname RecipientAddress -NotePropertyValue $ml.RecipientAddress
-             $value+=$obj
             }
-           }
-
-
-return $value
+Write-host "Hello!!!!"
+return $mail 
 } 
   
 
@@ -91,7 +61,7 @@ while($z -gt 0)
     while($i  -lt $pages)
 {     
       $mail+=Get-MessageTrace -Page $i -PageSize 5000 -MessageID $MessageID -startdate $date -enddate $enddate
-    $i++
+      $i++
       
 }
 $z=$z-1
@@ -114,12 +84,14 @@ while($i)
 $i=0
 if($mail.count -eq 1)
    {
+   Write-Host "The email eas received by single recipient $($mail.recipientaddress)"
+   $Subject=$Mail.Subject
    $mx=$null
-   $mx= MessageTrace  -RecipientAddress  $mail.RecipientAddress -Subject  $Subject  
-   
+   $msg= MessageTrace  -RecipientAddress  $mail.RecipientAddress -Subject  $Subject  
+   $mx=$msg |?{$_.subject -match $subject}
 }
 else
-   {
+   { $Subject=$Mail[0].Subject
      $mx=$null;$mx=@()
      foreach($ml in $mail)
         {
@@ -129,17 +101,20 @@ else
 $r=1
 while($r)
 {
+Write-Host "Inside the loop"
 $sender+=$mx |select SenderAddress
 $receiver+=$mx |select RecipientAddress
 #$receiver
 
 $mom=$null
+Write-host "Outside the loop"
 $check=$sender.SenderAddress + $receiver.RecipientAddress |group-object
+$check
 if ($check |?{$_.count -lt 2})
 {
 foreach($rec in $receiver)
       { 
-        Write-host "Inside the loop"
+        Write-host "Inside the loop !!!!!!!"
         $y=$rec.RecipientAddress
         $z=$sender |?{$_.SenderAddress -match $y}
         if($z.count -eq 0)
@@ -176,9 +151,9 @@ Write-Host "Email not found"
 
 Write-Host "Preparing Report........................" -ForegroundColor Green
 
-$data =import-csv $Resultfile
+$data =$mx|?{$_.subject -match $subject}
 
-del $Resultfile
+
 foreach($mail in $data)
 {
 $sender=($mail).'SenderAddress'
