5e4cefe676b3837f38b986815dbb65e5288bf46b
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-02-20 22:12:37 +0800

    Update messagetracedomo.ps1

--- messagetracedomo.ps1
+++ messagetracedomo.ps1
@@ -1,7 +1,7 @@
 # Step 1: Create a list of email addresses and subjects
 $emailList = @(
-    @{Address="user1@example.com"; Subject="Email Subject 1"},
-    @{Address="user2@example.com"; Subject="Email Subject 2"}
+    @{Address="user1@example.com"; Subject="*Report*"},
+    @{Address="user2@example.com"; Subject="*Report*"}
 )
 
 # Step 2-7: Perform message trace for each email address and subject, and drill down to all recipients
@@ -9,7 +9,7 @@ foreach ($email in $emailList) {
     $recipients = @($email.Address)
 
     do {
-        $results = Get-MessageTrace -RecipientAddress $recipients -MessageSubject $email.Subject
+        $results = Get-MessageTrace -RecipientAddress $recipients -MessageSubject $email.Subject -ResultSize 500
         $recipients = $results.RecipientAddress | Select-Object -Unique
     } while ($recipients)
 
