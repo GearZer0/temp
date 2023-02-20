34a039a90766867a31ef9c3419d74cf0f244be65
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-02-20 22:35:53 +0800

    Update messagetracedomo.ps1

--- messagetracedomo.ps1
+++ messagetracedomo.ps1
@@ -1,18 +1,61 @@
-# Step 1: Create a list of email addresses and subjects
-$emailList = @(
-    @{Address="user1@example.com"; Subject="*Report*"},
-    @{Address="user2@example.com"; Subject="*Report*"}
-)
+# Replace these with your own values
+$SenderEmailAddress = "sender@example.com"
+$EmailSubject = "Your email subject"
 
-# Step 2-7: Perform message trace for each email address and subject, and drill down to all recipients
-foreach ($email in $emailList) {
-    $recipients = @($email.Address)
+# Get the message trace results for the sender's email address and email subject
+$MessageTraceResults = Get-MessageTrace -SenderAddress $SenderEmailAddress -MessageSubject $EmailSubject
 
-    do {
-        $results = Get-MessageTrace -RecipientAddress $recipients -MessageSubject $email.Subject -ResultSize 500
-        $recipients = $results.RecipientAddress | Select-Object -Unique
-    } while ($recipients)
+# Loop through each message trace result
+foreach ($Result in $MessageTraceResults) {
+    # Get all the recipients for this message trace result
+    $Recipients = $Result.RecipientAddress
 
-    # Output the results for each email address and subject
-    Write-Host "Results for $($email.Address) - $($email.Subject): $($results.Count) messages found"
+    # Loop through each recipient
+    foreach ($Recipient in $Recipients) {
+        # Perform a message trace for this recipient
+        $RecipientTraceResults = Get-MessageTrace -RecipientAddress $Recipient -MessageSubject $EmailSubject
+
+        # Loop through each message trace result for this recipient
+        foreach ($RecipientResult in $RecipientTraceResults) {
+            # Get all the recipients for this recipient's message trace result
+            $SubRecipients = $RecipientResult.RecipientAddress
+
+            # Loop through each sub-recipient
+            foreach ($SubRecipient in $SubRecipients) {
+                # Perform a message trace for this sub-recipient
+                $SubRecipientTraceResults = Get-MessageTrace -RecipientAddress $SubRecipient -MessageSubject $EmailSubject
+
+                # If there are no results for this sub-recipient, move on to the next one
+                if ($SubRecipientTraceResults.Count -eq 0) {
+                    continue
+                }
+
+                # Loop through each message trace result for this sub-recipient
+                foreach ($SubRecipientResult in $SubRecipientTraceResults) {
+                    # Get all the recipients for this sub-recipient's message trace result
+                    $SubSubRecipients = $SubRecipientResult.RecipientAddress
+
+                    # Loop through each sub-sub-recipient
+                    foreach ($SubSubRecipient in $SubSubRecipients) {
+                        # Perform a message trace for this sub-sub-recipient
+                        $SubSubRecipientTraceResults = Get-MessageTrace -RecipientAddress $SubSubRecipient -MessageSubject $EmailSubject
+
+                        # If there are no results for this sub-sub-recipient, move on to the next one
+                        if ($SubSubRecipientTraceResults.Count -eq 0) {
+                            continue
+                        }
+
+                        # Loop through each message trace result for this sub-sub-recipient
+                        foreach ($SubSubRecipientResult in $SubSubRecipientTraceResults) {
+                            # Get all the recipients for this sub-sub-recipient's message trace result
+                            $SubSubSubRecipients = $SubSubRecipientResult.RecipientAddress
+
+                            # Loop through each sub-sub-sub-recipient (and so on)
+                            # ... (you get the idea)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
