5157e23ffbe872811c9414aab48dcc8bf639a7c3
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-05-17 12:16:07 +0800

    Update riskiq.py

--- riskiq.py
+++ riskiq.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 
 # Set up your RiskIQ API credentials
@@ -41,9 +42,20 @@ def retrieve_first_last_seen(domain):
         print(f"Status Code: {response.status_code}")
         print(f"Error Message: {response.text}")
 
-# List of domains to scan
-domains = ['example.com', 'google.com', 'github.com']
+# Read domains from a CSV file
+def read_domains_from_csv(filename):
+    with open(filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # Skip header row if present
+        domains = [row[0] for row in reader]
+    return domains
 
-# Iterate over the list and retrieve first and last seen dates
+# CSV file containing domains
+csv_filename = 'domains.csv'
+
+# Retrieve domains from CSV file
+domains = read_domains_from_csv(csv_filename)
+
+# Iterate over the domains and retrieve first and last seen dates
 for domain in domains:
     retrieve_first_last_seen(domain)
