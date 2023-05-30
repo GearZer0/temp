04c16ba1fdbd92493d9255b8efef03911b25a8b7
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2023-05-30 11:08:13 +0800

    Update test.c

--- test.c
+++ test.c
@@ -6,6 +6,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
 
 pid_t child_pid;
 
@@ -29,6 +32,42 @@ int main() {
 
         // Open the file in append mode and create it if it doesn't exist
         int file = open("test.txt", O_CREAT | O_WRONLY | O_APPEND, 0644);
+
+        // Create a Unix domain socket
+        int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            perror("Error creating socket");
+            exit(1);
+        }
+
+        // Bind the socket to a file
+        struct sockaddr_un address;
+        memset(&address, 0, sizeof(struct sockaddr_un));
+        address.sun_family = AF_UNIX;
+        strncpy(address.sun_path, "notmalicious.sock", sizeof(address.sun_path) - 1);
+
+        if (bind(sockfd, (struct sockaddr *)&address, sizeof(struct sockaddr_un)) == -1) {
+            perror("Error binding socket");
+            exit(1);
+        }
+
+        // Start listening for incoming connections
+        if (listen(sockfd, 5) == -1) {
+            perror("Error listening on socket");
+            exit(1);
+        }
+
+        // Accept incoming connections
+        struct sockaddr_un client_address;
+        socklen_t client_address_len = sizeof(client_address);
+        int client_sockfd = accept(sockfd, (struct sockaddr *)&client_address, &client_address_len);
+        if (client_sockfd == -1) {
+            perror("Error accepting connection");
+            exit(1);
+        }
+
+        // Redirect file output to the accepted socket
+        dup2(client_sockfd, file);
 
         // Keep the file open in the background
         while (1) {
